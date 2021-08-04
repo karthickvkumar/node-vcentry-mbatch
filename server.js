b70d42dc58aa74632ac5e1c5de0747b22ddb8dce
kvelraj_concord <kvelraj@concord.net>
2021-08-04 08:27:57 +0530

    sql

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const http = require('http').createServer(app);
 const bodyParse = require('body-parser');
 const mysql = require('mysql');
+const { response } = require('express');
+
+const DB_NAME = "student_db_test";
+const TABLE_NAME = "student_info";
 
 app.use(bodyParse.json());
 app.use(express.json());
@@ -22,85 +26,46 @@ connection.connect((error) => {
   console.log("Connected to the SQL Server");
 })
 
-const studentList = [{
-  firstName : "Yuvaraj",
-  lastName : "A",
-  rollNo : 4545,
-  age : 28,
-  id : 1
-},{
-  firstName : "Karthic",
-  lastName : "K",
-  rollNo : 4550,
-  age : 29,
-  id : 2
-},{
-  firstName : "Aswin",
-  lastName : "K",
-  rollNo : 4012,
-  age : 22,
-  id : 3
-}];
+app.post('/api/create/db', (request, response) => {
+  
+  connection.query(`CREATE DATABASE ${DB_NAME};`, (error, result) => {
+    if(error){
+      response.status(500).send({
+        message : "Error in creating the Database", error});
+      return;
+    }
+
+    const tableCreate = `USE ${DB_NAME}; CREATE TABLE ${TABLE_NAME} (first_name varchar(255), last_name varchar(255), age int(3), roll_no int(9))`;
+
+    connection.query(tableCreate, (error, result) => {
+      if(error){
+        response.status(500).send({
+          message : "Error in creating the Table", error});
+        return;
+      }
+
+      response.status(200).send("Successfully created a new Database and Table");
+    })
+    
+  });
+
+})
 
 app.get('/api/students', (request, response) => {
-  response.status(200).send(studentList);
+
 });
 
 app.post('/api/student/create', (request, response) => {
-  const newStudent = {
-    firstName : request.body.firstName,
-    lastName : request.body.lastName,
-    rollNo :request.body.rollNo,
-    age : request.body.age,
-    id : studentList.length + 1
-  }
-
-  studentList.push(newStudent);
-
-  const data = {
-    message : "User had been created successfully"
-  }
-
-  response.status(200).send(data);
+  
 });
 
 
 app.put('/api/student/edit/:id', (request, response) => {
-  const id = request.params.id;
-  const index = studentList.findIndex((value, index) => {
-    return value.id == id;
-  })
-
-  const editStudent = {
-    firstName : request.body.firstName,
-    lastName : request.body.lastName,
-    rollNo :request.body.rollNo,
-    age : request.body.age,
-    id : studentList[index].id
-  }
-
-  studentList[index] = editStudent;
-  const data = {
-    message : "User had been edited successfully"
-  }
-
-  response.status(200).send(data);
-
+ 
 })
 
 app.delete('/api/student/delete/:id', (request, response) => {
-  const id = request.params.id;
-  const index = studentList.findIndex((value, index) => {
-    return value.id == id;
-  })
-
-  studentList.splice(index, 1);
-
-  const data = {
-    message : "User had been deleted successfully"
-  }
-
-  response.status(200).send(data);
+  
 })
 
 
